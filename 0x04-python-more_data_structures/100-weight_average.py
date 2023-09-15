650384caca4100aedbd2e64ec9f7712356cb2800
AsmaaMohamed95 <asmaamostafa.pharmacist@gmail.com>
2023-09-15 15:35:40 -0700

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,7 +5,7 @@ def weight_average(my_list=[]):
     else:
         score, weight = 0, 0
         for i in range(len(my_list)):
-                score = score + my_list[i][0] * my_list[i][1]
-                weight = weight + my_list[i][1]
+            score = score + my_list[i][0] * my_list[i][1]
+            weight = weight + my_list[i][1]
         average = score / weight
     return average
