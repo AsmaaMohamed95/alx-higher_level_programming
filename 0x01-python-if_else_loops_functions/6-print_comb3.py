f1a896c21df0228f1b3f65f905bc8b1b28f51f7d
AsmaaMohamed95 <asmaamostafa.pharmacist@gmail.com>
2023-09-24 13:17:05 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for x in range(0, 10):
         if i < x and i != 8:
-            print(f"{i}{x}, ", end="")
+            print("{i}{x}, ".format(i, x), end="")
         elif i < x:
-            print(f"{i}{x}")
+            print("{i}{x}".format(i, x))
