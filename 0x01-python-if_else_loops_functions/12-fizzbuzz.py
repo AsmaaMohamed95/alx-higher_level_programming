444cab1463d0a4510f89606a6ac2e7754d592d68
AsmaaMohamed95 <asmaamostafa.pharmacist@gmail.com>
2023-09-25 13:44:53 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for x in range(0, 101):
         if x % 3 == 0:
-            if x % 3 == 0 and x % 5 == 0:
+            if x % 5 == 0:
                 print("FizzBuzz ", end="")
             else:
                 print("Fizz ", end="")
