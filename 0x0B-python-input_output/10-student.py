f9d1e889771694e1c95827749d4646f2a0efbcc7
AsmaaMohamed95 <asmaamostafa.pharmacist@gmail.com>
2023-10-11 14:26:26 -0700

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define class Student"""
+
+
+class Student:
+    """Blueprint for a student"""
+    def __init__(self, first_name, last_name, age):
+        """initialize a new student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of a Student.
+
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        """
+        if (type(attrs) != list or
+                any(type(ele) != str for ele in attrs)):
+            return self.__dict__
+
+        return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
